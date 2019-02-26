2c10a9a22c9d624239f094f627c914959eb6d149
Panagiotou <manopilipana97@gmail.com>
2019-02-26 02:08:53 +0200

    Now working

--- project/home/assets/js/pipes.js
+++ project/home/assets/js/pipes.js
@@ -1,16 +1,51 @@
-  const seconds = 3;
-  const transition = "linear";
+  //These change the output result
+  const seconds = 5;  //Total duration of the animation
+  const transition = "linear"; // Tranisiton animation of each path (not total)
   const max_lines = 50;
+  const widthPercentage = 0.2 // Width percentage on screen
+  const heightPercentage = 0.8 // Height percentage on screen
+  const scale = 1/3 // The bigger the denominator, the bigger the svg
+  const actual_margin_left = 12; // Affects first item of viebox
+  // end
+  var background_color = window.getComputedStyle(document.body)["background-color"];
+  var _ = require('underscore');
+  var svg = document.querySelector('#svgpipes');
+  var w = screen.width;
+  var h = screen.height;
+  svg.setAttribute('width', widthPercentage*w);
+  svg.setAttribute('height', heightPercentage*h);
+  svg.setAttribute("transform", "scale(1.0)");
+  vbox = svg.getAttribute('viewBox').trim().split(" ");
+  vbox[0] = actual_margin_left;
+  newbox = vbox.join(" ");
+  svg.setAttribute('viewBox', newbox);
+  var headertop = document.getElementById('header').clientHeight;
+  svg.style.marginLeft = 0;
+  svg.style.marginTop = headertop;
+  //Listen for screen changes
+  window.addEventListener('resize', function(event){
+    headertop = document.getElementById('header').clientHeight;
+    var w = screen.width;
+    var h = screen.height;
+    svg.setAttribute('width', widthPercentage*w);
+    svg.setAttribute('height', heightPercentage*h);
+    svg.setAttribute("transform", "scale(1.0)");
+    svg.style.marginLeft = 0;
+    svg.style.marginTop = headertop;
+    vbox = svg.getAttribute('viewBox').trim().split(" ");
+    vbox[0] = actual_margin_left;
+    newbox = vbox.join(" ");
+  });
 function animate_paths() {
           var paths = document.querySelectorAll('path');
           var TotalLengths = new Array(max_lines).fill(0); // TotalLength of line
           var allLines = 0;
           var groups = new Array(max_lines).fill(0); // keeps number of groups[line] = # of groups in line
           for (i = 0; i < paths.length; ++i) {
-            if(paths[i].getAttribute('fill') !="#FFFFFF"){
-              var gr = paths[i].getAttribute('g')
-              var li = paths[i].getAttribute('l');
-              if(gr > groups[li]){
+            if(paths[i].style['fill'] !="rgb(255, 255, 255)"){
+              gr = paths[i].getAttribute('g')*1;
+              li = paths[i].getAttribute('l');
+              if(gr >= groups[li]){
                 groups[li] = gr;
               }
               if(li > allLines ){
@@ -22,21 +57,29 @@ function animate_paths() {
               paths[i].style.strokeDasharray = len + ',' + len;
               TotalLengths[li] += len;
             }
+            else{
+              paths[i].style['fill'] = background_color;
+            }
           }
           // Set transition delay for each path
           for(line=0; line <=allLines ; line++ ){
             var ttw = 0;
             for (i = 0; i <=groups[line]; ++i) {
+
               path_group = document.querySelectorAll(`[g="${i}"][l="${line}"]`);
               for (j = 0; j < path_group.length; ++j) {
                 mylength = path_group[j].getTotalLength();
                 time_to_excecute = (mylength/TotalLengths[line]);
                 path_group[j].style['transition-delay'] = ttw*seconds + "s";
-                console.log(`Path g= ${i} l= ${line} waits ${ttw*seconds}`)
               }
               ttw += time_to_excecute;
             }
           }
+          /*
+          window.onscroll =  _.debounce(function() {
+              animate(TotalLengths);
+          }, 250);
+          */
           animate(TotalLengths);
     }
 
@@ -61,4 +104,5 @@ function animate_paths() {
           paths[i].style.strokeDashoffset = '0';
         }
     }
-    animate_paths();
+
+animate_paths();
